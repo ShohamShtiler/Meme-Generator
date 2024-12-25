1a005d639568e0f90e490d0ab65c543b9760b991
Shoham Shtiler <shtilershoham@gmail.com>
2024-12-25 21:34:49 +0200

    Add about section

--- js/services/utils-service.js
+++ js/services/utils-service.js
@@ -8,6 +8,8 @@ function handleRouting() {
     showMemeEditor()
   } else if (route === 'saved') {
     showSavedMemes()
+  } else if (route === 'about') {
+    showAbout()
   } else {
     showGallery()
   }
@@ -17,19 +19,30 @@ function showGallery() {
   document.querySelector('.main-gallery').style.display = 'grid'
   document.querySelector('.meme-editor').style.display = 'none'
   document.querySelector('.saved').style.display = 'none'
+  document.querySelector('.about').style.display = 'none'
 }
 
 function showMemeEditor() {
   document.querySelector('.main-gallery').style.display = 'none'
   document.querySelector('.meme-editor').style.display = 'block'
   document.querySelector('.saved').style.display = 'none'
+  document.querySelector('.about').style.display = 'none'
 
 }
 
 function showSavedMemes() {
   document.querySelector('.main-gallery').style.display = 'none'
   document.querySelector('.meme-editor').style.display = 'none'
+  document.querySelector('.about').style.display = 'none'
   document.querySelector('.saved').style.display = 'grid'
+}
+
+function  showAbout() {
+  document.querySelector('.main-gallery').style.display = 'none'
+  document.querySelector('.meme-editor').style.display = 'none'
+  document.querySelector('.saved').style.display = 'none'
+  document.querySelector('.about').style.display = 'block'
+
 }
 
 function toggleMenu() {
