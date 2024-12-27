8ba34c06d7093a3e94ce3954448f27a9af3160f6
Shoham Shtiler <shtilershoham@gmail.com>
2024-12-27 17:41:03 +0200

    Add keys and fix mobile-layout

--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -2,20 +2,20 @@
 
 var gImgs = [
     { id: 1, url: 'images/1.jpg', keywords: ['funny', ''] },
-    { id: 2, url: 'images/2.jpg', keywords: ['dog', ''] },
+    { id: 2, url: 'images/2.jpg', keywords: ['dog', 'animal'] },
     { id: 3, url: 'images/3.jpg', keywords: ['baby', 'dog'] },
-    { id: 4, url: 'images/4.jpg', keywords: ['', 'cat'] },
+    { id: 4, url: 'images/4.jpg', keywords: ['animal', 'cat'] },
     { id: 5, url: 'images/5.jpg', keywords: ['baby', ''] },
     { id: 6, url: 'images/6.jpg', keywords: ['funny', ''] },
     { id: 7, url: 'images/7.jpg', keywords: ['funny', 'baby'] },
-    { id: 8, url: 'images/8.jpg', keywords: ['smile', ''] },
+    { id: 8, url: 'images/8.jpg', keywords: ['funny', ''] },
     { id: 9, url: 'images/9.jpg', keywords: ['funny', 'baby'] },
     { id: 10, url: 'images/10.jpg', keywords: ['funny', ''] },
-    { id: 11, url: 'images/11.jpg', keywords: ['funny', ''] },
-    { id: 12, url: 'images/12.jpg', keywords: ['funny', 't'] },
-    { id: 13, url: 'images/13.jpg', keywords: ['funny', ''] },
-    { id: 14, url: 'images/14.jpg', keywords: ['funny', ''] },
-    { id: 15, url: 'images/15.jpg', keywords: ['funny', ''] },
+    { id: 11, url: 'images/11.jpg', keywords: ['serious', ''] },
+    { id: 12, url: 'images/12.jpg', keywords: ['serious', ''] },
+    { id: 13, url: 'images/13.jpg', keywords: ['serious', ''] },
+    { id: 14, url: 'images/14.jpg', keywords: ['serious', ''] },
+    { id: 15, url: 'images/15.jpg', keywords: ['serious', ''] },
     { id: 16, url: 'images/16.jpg', keywords: ['funny', ''] },
 ]
 
